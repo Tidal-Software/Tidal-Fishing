bf2a4a53d1dc0e423ec434097bd002b2dcbdcc96
John Parkhurst <jparkhurst120@gmail.com>
2022-03-03 23:23:35 -0500

    Phaser

--- src/base.js
+++ src/base.js
@@ -24,15 +24,20 @@ function preload ()
 {
     console.log("Loading Images");
     this.load.image('sky', "./src/assets/sky.png");
+    this.load.image('star',"./src/assets/star.png")
     console.log("Completed Loading Images");
 }
 //On creation
+var x = 400;
+var y = 300;
 function create () {
+    //With phaser positioning is from center so 800/600 would be 400/300 to center
     this.add.image(400,300,'sky');
+    this.add.image(x, y, 'star');
     const particles = this.add.particles('red');
 
     const emitter = particles.createEmitter({
-        speed: 300,
+        speed: 500,
         scale: { start: 1, end: 0 },
         blendMode: 'ADD'
     });
@@ -41,6 +46,8 @@ function create () {
 //every frame
 function update ()
 {
+    x= this.input.mousePointer.x;
+    y = this.input.mousePointer.y;
 }
 
 var game = new Phaser.Game(config);
